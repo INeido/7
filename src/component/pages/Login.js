2733a7a67d07f5dd55bb3a9ecf83eccfb655dc70
INeido <reg@neido.de>
2022-06-02 21:03:24 +0200

    Connected Login to API

--- src/component/pages/Login.js
+++ src/component/pages/Login.js
@@ -7,23 +7,43 @@ import {
   ThemeProvider,
   InputAdornment,
   CssBaseline,
+  Skeleton,
 } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import { createGame, isRunning } from "../../logic/api";
 
 export default function _(props) {
-  const [btn_loading, setLoading] = React.useState(false);
-  const [field_disabled, setDisabled] = React.useState(false);
-  const btn_text = props.mode === "create" ? "Create" : "Join";
+  const [btnLoading, setBtnLoading] = React.useState(false);
+  const [btnText, setBtnText] = React.useState("Join");
+  const [fieldDisabled, setFieldDisabled] = React.useState(false);
+  const [pageLoading, setPageLoading] = React.useState(true);
+  const [newGame, setNewGame] = React.useState(false);
+  const [gameID, setGameID] = React.useState(0);
+
+  isRunning().then((res) => {
+    setNewGame(Boolean(res.data[0].locked));
+    if (newGame) {
+      setBtnText("Create");
+    } else {
+      setGameID(res.data[0].game_id);
+    }
+    setPageLoading(false);
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   function onSubmit(data) {
-    setLoading(true);
-    setDisabled(true);
-    console.log(data);
+    setBtnLoading(true);
+    setFieldDisabled(true);
+    if (newGame) {
+      createGame().then((res) => {
+        setGameID(res.data.insertId);
+      });
+    }
   }
 
   return (
@@ -37,39 +57,46 @@ export default function _(props) {
           alignItems="center"
           sx={{ marginTop: "40%" }}
         >
-          <Paper variant="outlined" sx={{ mt: 1, px: 2, py: 2 }}>
-            <TextField
-              error={errors.player_name ? true : false}
-              {...register("player_name", { required: true })}
-              disabled={field_disabled}
-              fullWidth
-              autoFocus
-              helperText={errors.player_name ? "Bitte Namen eingeben." : ""}
-              margin="normal"
-              name="player_name"
-              label="Name"
-              type="text"
-              id="player_name"
-              variant="standard"
-              color="primary"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <LoadingButton
-              loading={btn_loading}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3 }}
+          {pageLoading ? (
+            <Skeleton variant="rectangular" width={210} height={118} />
+          ) : (
+            <Paper
+              variant="outlined"
+              sx={{ mt: 1, px: 2, py: 2, maxWidth: 300 }}
             >
-              {btn_text}
-            </LoadingButton>
-          </Paper>
+              <TextField
+                error={errors.player_name ? true : false}
+                {...register("player_name", { required: true })}
+                disabled={fieldDisabled}
+                fullWidth
+                autoFocus
+                helperText={errors.player_name ? "Bitte Namen eingeben." : ""}
+                margin="normal"
+                name="player_name"
+                label="Name"
+                type="text"
+                id="player_name"
+                variant="standard"
+                color="primary"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <LoadingButton
+                loading={btnLoading}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3 }}
+              >
+                {btnText}
+              </LoadingButton>
+            </Paper>
+          )}
         </Grid>
       </form>
     </ThemeProvider>
