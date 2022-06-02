e5eeda8459e1fb1336e652a45bab97945cb2732b
INeido <reg@neido.de>
2022-06-02 21:03:49 +0200

    Finalized GETs

--- src/logic/api.js
+++ src/logic/api.js
@@ -1,14 +1,33 @@
 const axios = require("axios").default;
 
-export default function api() {
-  axios
-    .get("/db")
-    .then(function (response) {
-      console.log(response);
-      return response;
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {});
+export function createGame() {
+  return axios.get("/creategame");
+}
+
+export function closeGame() {
+  return axios.get("/closegame");
+}
+
+export function isRunning() {
+  return axios.get("/isrunning");
+}
+
+export function getPlayer(data) {
+  axios.post("/getplayer", { userName: data.player_name });
+}
+
+export function createPlayer(data) {
+  axios.post("/createplayer", { userName: data.player_name });
+}
+
+export function updatePlayer(data) {
+  axios.post("/updateplayer", { userName: data.player_name });
+}
+
+export function isAdmin(data) {
+  axios.post("/isadmin", { userName: data.player_name });
+}
+
+export function isPlayer(data) {
+  axios.post("/isplayer", { userName: data.player_name });
 }
