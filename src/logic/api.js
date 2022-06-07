188cd4da6b9cce7946c1de8f0ad30fcd30e05770
INeido <reg@neido.de>
2022-06-07 20:33:26 +0200

    Fixed API for production

--- src/logic/api.js
+++ src/logic/api.js
@@ -1,49 +1,51 @@
 const axios = require("axios").default;
 
 export function createGame() {
-  return axios.get("/creategame");
+  return axios.get("https://neido.tech/wonder/creategame");
 }
 
 export function closeGame() {
-  return axios.get("/closegame");
+  return axios.get("https://neido.tech/wonder/closegame");
 }
 
 export function isRunning() {
-  return axios.get("/isrunning");
+  return axios.get("https://neido.tech/wonder/isrunning");
 }
 
 export function getPlayer(player_id) {
-  return axios.post("/getplayer", {
+  return axios.post("https://neido.tech/wonder/getplayer", {
     player_id: player_id,
   });
 }
 
 export function createPlayer(game_id, player_name) {
-  return axios.post("/createplayer", {
+  return axios.post("https://neido.tech/wonder/createplayer", {
     game_id: game_id,
     player_name: player_name,
   });
 }
 
 export function updatePlayer(data) {
-  return axios.post("/updateplayer", {
+  return axios.post("https://neido.tech/wonder/updateplayer", {
     data: data,
   });
 }
 
 export function isAdmin(data) {
-  return axios.post("/isadmin", { userName: data.player_name });
+  return axios.post("https://neido.tech/wonder/isadmin", {
+    userName: data.player_name,
+  });
 }
 
 export function isPlayer(game_id, player_name) {
-  return axios.post("/isplayer", {
+  return axios.post("https://neido.tech/wonder/isplayer", {
     game_id: game_id,
     player_name: player_name,
   });
 }
 
 export function getScores(game_id) {
-  return axios.post("/getscores", {
+  return axios.post("https://neido.tech/wonder/getscores", {
     game_id: game_id,
   });
 }
